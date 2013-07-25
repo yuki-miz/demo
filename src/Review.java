21b0b4e264b60cb19958c982d91b6e7449deda77
Mizushima <yuuki.mizushima@nts.ricoh.co.jp>
2013-07-25 16:50:56 +0900

    本日の日付をほしいフォーマットで返すメソッドgetTodayString()を作成
    Close #2053

--- src/Review.java
+++ src/Review.java
@@ -1,7 +1,15 @@
 import java.io.*;
+import java.util.Date;
+import java.text.*;
 
 class Review {
 	public static void main(String[] args){
-		System.out.println("hello");
+		System.out.println(getTodayString());
+	}
+
+	public static String getTodayString(){
+		Date now = new Date();
+		SimpleDateFormat format = new SimpleDateFormat("yyyy_MM_dd");
+		return format.format(now);
 	}
 }
